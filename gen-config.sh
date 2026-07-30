294fda8b40b0fa4700ad18941f77a01885c1d122
Ilya Tumaykin <itumaykin@gmail.com>
2026-07-30 07:31:10 +0300

    server: separate Vision and mux clients
    Mux prevents Vision from working properly. They can be both enabled
    on the same inbound but UUIDs must be different with different flows.

--- gen-config.sh
+++ gen-config.sh
@@ -7,7 +7,8 @@ _REALITY_KEYS="$(sing-box generate reality-keypair)"
 REALITY_PRIVATE_KEY="$(echo "${_REALITY_KEYS}" | awk '/PrivateKey:/ {print $2}')"
 REALITY_PUBLIC_KEY="$(echo "${_REALITY_KEYS}" | awk '/PublicKey:/ {print $2}')"
 
-VLESS_USER_UUID="$(sing-box generate uuid)"
+VLESS_USER_UUID_1="$(sing-box generate uuid)"
+VLESS_USER_UUID_2="$(sing-box generate uuid)"
 
 SALAMANDER_OBFS_PASSWORD="$(sing-box generate rand 24 --base64)"
 
@@ -21,7 +22,8 @@ sed -i \
     -e "s|SERVER_IP|${SERVER_IP}|g" \
     -e "s|REALITY_PUBLIC_KEY|${REALITY_PUBLIC_KEY}|g" \
     -e "s|REALITY_SHORT_ID|${REALITY_SHORT_ID}|g" \
-    -e "s|VLESS_USER_UUID|${VLESS_USER_UUID}|g" \
+    -e "s|VLESS_USER_UUID_1|${VLESS_USER_UUID_1}|g" \
+    -e "s|VLESS_USER_UUID_2|${VLESS_USER_UUID_2}|g" \
     -e "s|SALAMANDER_OBFS_PASSWORD|${SALAMANDER_OBFS_PASSWORD}|g" \
     -e "s|HYSTERIA2_USER_PASSWORD|${HYSTERIA2_USER_PASSWORD}|g" \
     -e "s|HYSTERIA2_CERT_HASH|${HYSTERIA2_CERT_HASH}|g" \
@@ -30,7 +32,8 @@ sed -i \
 sed -i \
     -e "s|REALITY_PRIVATE_KEY|${REALITY_PRIVATE_KEY}|g" \
     -e "s|REALITY_SHORT_ID|${REALITY_SHORT_ID}|g" \
-    -e "s|VLESS_USER_UUID|${VLESS_USER_UUID}|g" \
+    -e "s|VLESS_USER_UUID_1|${VLESS_USER_UUID_1}|g" \
+    -e "s|VLESS_USER_UUID_2|${VLESS_USER_UUID_2}|g" \
     -e "s|SALAMANDER_OBFS_PASSWORD|${SALAMANDER_OBFS_PASSWORD}|g" \
     -e "s|HYSTERIA2_USER_PASSWORD|${HYSTERIA2_USER_PASSWORD}|g" \
     server/config.json
