08813429de4f2b59da8f4d9354f31547ab307d6f
Ilya Tumaykin <itumaykin@gmail.com>
2026-08-02 04:06:02 +0300

    Adjust domains to avoid long certificate chains
    www.microsoft.com has certificate chain length greater than sing-box's
    internal limit 8192 bytes. This makes perfectly valid setup fail during
    the REALITY handshake.
    
    Change domains to make certificate chains shorter for VLESS+REALITY and
    to properly support HTTP/3 for Hysteria2 (although OBFS currently makes
    this irrelevant as the domain is never seen on the wire).

--- server/hysteria2/gen-hysteria2-cert.sh
+++ server/hysteria2/gen-hysteria2-cert.sh
@@ -3,5 +3,5 @@
 openssl req -x509 -newkey rsa:2048 -sha384 -days 3650 -noenc \
     -keyout server.key \
     -out server.crt \
-    -subj "/CN=www.microsoft.com" \
-    -addext "subjectAltName=DNS:www.microsoft.com"
+    -subj "/CN=cloud.google.com" \
+    -addext "subjectAltName=DNS:cloud.google.com"
